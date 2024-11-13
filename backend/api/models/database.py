28e9fa8ebf5c99ed6fe286acd71397086bab15cb
hugocrt <hugo.carangeot@edu.esiee.fr>
2024-11-13 18:10:19 +0100

    correction 1 docker compose

--- backend/api/models/database.py
+++ backend/api/models/database.py
@@ -13,7 +13,7 @@ load_dotenv()
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "postgres")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 
 if not all([POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB]):
